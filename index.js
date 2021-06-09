9f82a6fd0a5735c4f5b94d151091ea48d3f777e1
Min Latt <minminlaxz@gmail.com>
2021-06-10 02:53:42 +0630

    more get routes

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ app.use(express.static(__dirname + '/public'));
 
 const handleError = (err, res) => {
     console.log(err)
-    res
-        .status(500)
-        .contentType("text/plain")
-        .end(`Informative Debugger : Oops! Something went wrong!\n${err}`);
+    // res
+    //     .status(500)
+    //     .contentType("text/plain")
+    //     .end(`Informative Debugger : Oops! Something went wrong!\n${err}`);
+    res.sendFile(`${__dirname}/public/error.html`)
 };
 
 
@@ -42,57 +43,23 @@ app.get("/", (req, res) => {
     res.render('/index.html')
 })
 
+app.get("/success", (req, res) => {
+    res.sendFile(`${__dirname}/public/success.html`)
+})
+
+
+app.get("/error", (req, res) => {
+    res.sendFile(`${__dirname}/public/error.html`)
+})
+
 app.post('/upload', function (req, res, next) {
     upload(req, res, function (err) {
         if (err) { return handleError(err, res); }
-        res.end("Upload completed.");
+        res.redirect("/success");
+        // res.end("Upload completed.");
     });
 })
 
-// const upload = multer({
-//     dest: "/home/laxz/exp-multr/"
-//     // you might also want to set some limits: https://github.com/expressjs/multer#limits
-// });
-
-// app.post("/upload",
-//     upload.single("file" /* name attribute of <file> element in your form */),
-//     (req, res) => {
-//         const tempPath = req.file.path; // /home/laxz/exp-multr/af8de3bac38e06452c14135de1a8f387 
-//         let targetPath = null
-
-//         const fileExt = path.extname(req.file.originalname).toLowerCase();
-//         switch (fileExt) {
-//             case ".png":
-//                 targetPath = path.join(__dirname, `./uploads/${getTimeStamp()}${fileExt}`);
-//                 break;
-//             case ".jpg":
-//             case ".jpeg":
-//                 targetPath = path.join(__dirname, `./uploads/${getTimeStamp()}${fileExt}`);
-//                 break;
-//             default:
-//                 targetPath = null;
-//         }
-
-//         if (targetPath !== null) {
-//             fs.rename(tempPath, targetPath, err => { // /usr/src/app/uploads/1623255325495.jpg
-//                 if (err) return handleError(err, res);
-//                 res
-//                     .status(200)
-//                     .contentType("text/plain")
-//                     .end("File uploaded!");
-//             });
-//         } 
-//         else {
-//             fs.unlink(tempPath, err => {
-//                 if (err) return handleError(err, res);
-//                 res
-//                     .status(403)
-//                     .contentType("text/plain")
-//                     .end(`Only .jpg or .jpeg and .png files are allowed!\nAnd your file type is : ${fileExt}\nwhich is not supposed to be uploaded.`);
-//             });
-//         }
-//     }
-// );
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
